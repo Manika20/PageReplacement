57da9d11be0fcb986b52f0f08ad12f2e45504825
manika <goyalmanika20@gmail.com>
2024-10-10 21:07:25 +0530

    build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -1,11 +1,28 @@
-#!/bin/bash
+@echo off
+setlocal
 
-# Compile C++ files
+REM Compile C++ files
 g++ -o fifo cpp/fifo.cpp
 g++ -o lru cpp/lru.cpp
 g++ -o mru cpp/mru.cpp
 g++ -o optimal cpp/optimal.cpp
 g++ -o lfu cpp/lfu.cpp
 
-# Move executables to the backend folder
-mv fifo lru mru optimal lfu ./
+REM Check if the executables exist and move them if they don't
+if not exist fifo.exe (
+    move fifo.exe .\
+)
+if not exist lru.exe (
+    move lru.exe .\
+)
+if not exist mru.exe (
+    move mru.exe .\
+)
+if not exist optimal.exe (
+    move optimal.exe .\
+)
+if not exist lfu.exe (
+    move lfu.exe .\
+)
+
+endlocal
